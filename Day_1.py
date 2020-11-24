cf56996a9261845b03b549935818ded272852872
amani021 <003a48@gmail.com>
2020-11-24 23:18:39 +0300

    first commit

---
+++ Day_1.py
@@ -0,0 +1,5 @@
+# -------- DAY 1 "Basic Info" --------
+
+country = input("Where are from? ")
+num = input("What is your favorite number? ")
+print('You are from', country, 'and your favorite number - 1 is', int(num)-1)
