36afb33ec1daa3a422e8991d2acf14fbeb68b954
amani021 <003a48@gmail.com>
2020-11-25 23:45:19 +0300

    Few changes

--- Day_1.py
+++ Day_1.py
@@ -1,5 +1,7 @@
 # -------- DAY 1 "Basic Info" --------
-
-country = input("Where are from? ")
-num = input("What is your favorite number? ")
-print('You are from', country, 'and your favorite number - 1 is', int(num)-1)
+# Goal: Learn variables, data types, inputs/outputs, convert from string to number, print with f-string
+#     and how to deal with GitHub.
+print('`\\__ Hello World, it is iPython __/`')
+country = input("Where are you from?\n")
+num = input("What is your favorite number?\n")
+print(f'~~~~~~~~~~~~\nYou are from {country} and your favorite number - 1 is {int(num)-1}')
