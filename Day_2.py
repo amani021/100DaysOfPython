2bcf29f12392feb327c91b32737073890c4a0aa0
amani021 <003a48@gmail.com>
2020-11-26 00:30:34 +0300

    Calculate the tax value

--- Day_2.py
+++ Day_2.py
@@ -2,6 +2,7 @@
 # Goal: Learn more about the day 1's goal.
 print('Welcome to iPython Supermarket')
 bill = float(input('How much is your bill? ... '))
+tax = bill*(15/100)
 print(f'TAX is 15%\nBill is {bill}')
-print(f'~~~~~~~~~~~~\nThe total including the TAX is {bill*(15/100)}$')
+print(f'~~~~~~~~~~~~\nThe total including the TAX is {bill+tax}$')
 print('THANK YOU, SEE YOU AGAIN ^-^')
